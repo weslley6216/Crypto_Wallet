24d7715b1cc48dc31663c93c251d4be4341fc328
Weslley Campos <weslley.camphos@hotmail.com>
2021-05-07 18:39:27 -0300

    Aula  sobre cookies e sessions

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,4 +1,7 @@
 class WelcomeController < ApplicationController
   def index
+    cookies[:curso] = 'Curso de Ruby on Rails [COOKIE]'
+    session[:session] = 'Curso de Ruby on Rails [SESSION]'
+    @meu_nome = params[:nome]
   end
 end
