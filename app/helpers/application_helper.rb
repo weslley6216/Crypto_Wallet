74c7668977a0c439fd6b09e0dc7677cc4d8a199c
Weslley Campos <weslley.camphos@hotmail.com>
2021-05-07 18:40:41 -0300

    Criaçao do helper ambiente_rails

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,21 @@
 module ApplicationHelper
 
+  def locale
+    I18n.locale == :en ? 'Estados Unidos' : 'Português do Brasil'
+  end
+
   def br_date(data_us)
-    data_us.strftime('%d/%m/%')
+    data_us.strftime('%d/%m/%Y')
   end
   
+  def ambiente_rails
+    if Rails.env.development?
+      'Desenvolvimento'
+    elsif Rails.env.production?
+      'Produção'
+    else
+      'Teste'
+    end
+
+  end
 end
